434df32f8026dc0a34cc9417bc023f9326e536d5
Carlos William <carlos_william_santos@hotmail.com>
2019-10-18 02:00:46 -0300

    cadastro e login com firebase

--- firebase/src/App.js
+++ firebase/src/App.js
@@ -8,13 +8,31 @@ export default class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      token:"...",
-      inputName: null, 
-      inputIdade: null,
+      cadastroName: null, 
+      cadastroEmail: null,
+      cadastroSenha: null,
+      loginNome: null,
+      loginEmail: null,
+      loginSenha: null,
       lista: []
     }
 
     this.cadastrar = this.cadastrar.bind(this);
+    this.login = this.login.bind(this);
+    this.deslogar = this.deslogar.bind(this);
+
+    firebase.auth().onAuthStateChanged((user) =>{
+      if(user){
+        alert("Usuario logado com suceso!")
+        this.setState({
+          loginEmail: user.email
+        })
+        
+      }
+    })
+
+
+  }
 
   // Insert novo valor
   // firebase.database().ref("ativo").set("sim cadastrou")
@@ -34,69 +52,74 @@ export default class App extends Component{
   //   this.setState(state)
   // })
 
-firebase.database().ref("usuarios").on("value", snapshot =>{
-  let state = this.state;
-  state.lista = [];
-
-  snapshot.forEach((item) =>{
-    state.lista.push({
-      key: item.key,
-      nome: item.val().nome,
-      idade: item.val().idade
-    })
-  })
-
-  this.setState(state);
-})
-  }
-
   cadastrar(e){
     e.preventDefault();
 
-    let usuarios = firebase.database().ref("usuarios");
-    let chave = usuarios.push().key;
-
-    usuarios.child(chave).set({
-      nome: this.state.inputName,
-      idade: this.state.inputIdade
+    firebase.auth().createUserWithEmailAndPassword(this.state.cadastroEmail, this.state.cadastroSenha).then().catch((er) =>{
+      if(er.code === "auth/invalid-email"){
+        alert("E-mail Invalido");
+      } else if(er.code === "auth/email-already-in-use"){
+       alert("E-mail ja esta sendo utilizado") 
+      } else{
+        alert(er.code)
+      }
     })
   }
 
   
+  login(e){
+    e.preventDefault();
+
+    firebase.auth().signInWithEmailAndPassword(this.state.loginEmail, this.state.loginSenha).then().catch((er) =>{
+      if(er.code === "auth/invalid-email"){
+        alert("E-mail Invalido");
+      } else if(er.code === "auth/email-already-in-use"){
+       alert("E-mail ja esta sendo utilizado") 
+      } else{
+        alert(er.code)
+      }
+    })
+  }
+
+  deslogar(){
+    firebase.auth().signOut().then(
+      alert("deslogado")
+    )
+
+    this.setState({
+      loginEmail: null
+    })
+  }
 
   render(){
 
     return(
       <div className="container">
+        <div className="form-cadastrar"> 
+            <div>
+              <h2>Logado com email: {this.state.loginEmail}</h2>
+              <button onClick={this.deslogar}>Deslogar</button>
+            </div>
+        <h2>Cadastro</h2>
+          <form onSubmit={this.cadastrar}>
+          {/* <form onSubmit={() => this.cadastrar(e.target)}> */}
 
-      {
-        this.state.lista.map((item) =>{
-          return(
-            <article key={item.key}>
-              <h5>Chave: {item.key}</h5>
-              <h5>Nome: {item.nome}</h5>
-              <h5>idade: {item.idade}</h5>
-              
-            </article>
-          );
-        })
-      }
+            <input type="text" placeholder="Nome" value={this.cadastroNomel} onChange={(e) => {this.setState({cadastroNome: e.target.value})}} />
+            <input type="text" placeholder="E-mail" value={this.cadastroEmail} onChange={(e) => {this.setState({cadastroEmail: e.target.value})}} />            
+            <input type="text" placeholder="Senha" value={this.cadastroSenha} onChange={(e) => {this.setState({cadastroSenha: e.target.value})}} />
+            <button type="submit">Cadastrar</button>
+          </form>
+        </div>
+        <div className="form-login">
+        <h2>Login</h2>
+        <form onSubmit={this.login}>
+          {/* <form onSubmit={() => this.cadastrar(e.target)}> */}
 
-
-      <form onSubmit={this.cadastrar}>
-          <input type="text"  onChange={(e) =>{
-            this.setState({
-              inputName: e.target.value
-            })
-          }} />
-          <input type="text" onChange={(e) =>{
-            this.setState({
-              inputIdade: e.target.value
-            })
-          }} /><br></br>
-          <button type="submit">Cadastrar no firebase filhao</button>
-          <br></br>
-      </form>
+            <input type="email" placeholder="E-mail" value={this.loginEmail} onChange={(e) => {this.setState({loginEmail: e.target.value})}} />            
+            <input type="text" placeholder="Senha" value={this.loginSenha} onChange={(e) => {this.setState({loginSenha: e.target.value})}} />
+            <button type="submit">Cadastrar</button>
+          </form>
+        </div>
 
       </div>
     );
